26d5fc54002b8389776b05238e05fa3d3fb60cf8
sakineh-sp <sakineh.mehrabi@smartpension.co.uk>
2018-08-04 22:03:09 +0100

    collapse

--- src/App.js
+++ src/App.js
@@ -1,55 +1,74 @@
 import React, { Component } from 'react';
 import './stylesheet.css';
-import List from './component/list';
+import Collapsible from './Collapsible';
+// import List from './component/list';
+
+
 
 
 class App extends Component {
-  constructor(){
-    super() ;
-    this.state = {
-      contact :[]
   
-    }
-  }
-  componentDidMount(){
-    this.fetchData();
 
-  }
-  fetchData(){
-    fetch('https://randomuser.me/api/?results=20&nat=us,dk,fr,gb')
-    .then(response =>response.json())
-    .then(parsedJSON =>parsedJSON.results.map(user=>(
-      {
-        name:'${user.name.first}${user.name.last}',
-        username:'${user.login.username}',
-        email:'${user.email}',
-        location:'${user.location.street}${user.location.city}',
-      }
+//   constructor(){
+//     super() ;
+//     this.state = {
+//       contacts :[]
+  
+//     }
+//   }
+//   componentDidMount(){
+//     this.fetchData();
 
-    )))
-    .then(contacts=> this.setState({
-      contact:contacts
+//   }
+//   fetchData(){
+//     fetch('https://randomuser.me/api/?results=20&nat=us,dk,fr,gb')
+//     .then(response =>response.json())
+//     .then(parsedJSON =>parsedJSON.results.map(user=>(
+//       {
+//         name:'${user.name.first}${user.name.last}',
+//         username:'${user.login.username}',
+//         email:'${user.email}',
+//         location:'${user.location.street}${user.location.city}',
+//       }
 
+//     )))
+//     .then(contacts=> this.setState({
+//       contacts
+//     }))
 
-    }))
+//     .catch(error => console.log('parsing faild',error))
 
-    .catch(error => console.log('parsing faild',error))
-
-}
+// }
   
   render() {
+    // const contacts = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">API Test</h1>
-        </header>
-        <List contacts={this.state.contact}></List>
-        <p>
-          lkdfnhv kjhdusb  jnsbdiuch kjdbcb 
-        </p>
-      </div>
+      <div>
+         <header>
+            <h1>Collapsible Content</h1>
+          </header>
+          <div className="content">
+              <div className="panel-group">
+                <Collapsible></Collapsible>
+              </div>
+          </div>
+        {/* /* <h1>Collapse</h1> */}
+        {/* <div className="App">
+            <div className="title">
+                <h1 onClick={this.toggle} >HHHHHHHHHH</h1>
+                    <Collapse isOpen={this.state.collapse}>
+                          <Card body className="text-center" >
+                              <CardTitle className="title">Special Title Treatment</CardTitle>
+                              <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                              <Button outline color="secondary" >Go somewhere</Button>
+                          </Card>
+                    </Collapse>
+              </div>    
+        </div> */}
+        
+     </div> 
     );
   }
-}
+ }
 
 export default App;
---
+++ src/Collapsible.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// import { Collapse,Card, Button, CardTitle, CardText, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+class Collapsible extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {isExpanded:false};
+  }
+  handleToggle(e){
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    })
+  }
+  render(){
+    const {title,children}=this.props;
+    const {isExpanded}= this.state;
+    return(
+      <div className={`panel ${isExpanded ? 'is-expanded': ''}` } onClick={(e) => this.handleToggle(e)}>
+          <div className="panel-heading"><h2>{title}</h2>
+              </div>
+          <div className="panel-collapse">
+              <div className="panel-body">
+                  {children}
+              </div>
+          </div>
+      </div> 
+    )
+    
+  }
+}
+export default Collapsible;
